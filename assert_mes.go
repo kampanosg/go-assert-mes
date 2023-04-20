4a5bdf21be7c0d192de83a36b36e54babe472d23
George Kampanos <george.kampanos@nandos.co.uk>
2023-04-20 14:01:51 +0100

    check for exact match

--- assert_mes.go
+++ assert_mes.go
@@ -1,1 +1,37 @@
 package mes
+
+import (
+	"reflect"
+	"testing"
+)
+
+func MatchExactly(t *testing.T, expected, actual interface{}) {
+	if !exactMatch(expected, actual) {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func exactMatch(expected, actual interface{}) bool {
+	if expected == nil || actual == nil {
+		return false
+	}
+
+	if reflect.TypeOf(expected).Kind() != reflect.Slice || reflect.TypeOf(actual).Kind() != reflect.Slice {
+		return false
+	}
+
+	s1 := reflect.ValueOf(expected)
+	s2 := reflect.ValueOf(actual)
+
+	if s1.Len() != s2.Len() {
+		return false
+	}
+
+	for i := 0; i < s1.Len(); i++ {
+		if s1.Index(i).Interface() != s2.Index(i).Interface() {
+			return false
+		}
+	}
+
+	return true
+}
--- assert_mes_test.go
+++ assert_mes_test.go
@@ -1,1 +1,71 @@
-package mes_test
+package mes
+
+import (
+	"testing"
+)
+
+func TestExactMatch(t *testing.T) {
+	tests := []struct {
+		name     string
+		expected interface{}
+		actual   interface{}
+		want     bool
+	}{
+		{
+			name:     "nil slices",
+			expected: nil,
+			actual:   nil,
+			want:     false,
+		},
+		{
+			name:     "non slice types",
+			expected: 1,
+			actual:   "one",
+			want:     false,
+		},
+		{
+			name:     "different length slices",
+			expected: []int{1, 2, 3},
+			actual:   []int{1, 2, 3, 4},
+			want:     false,
+		},
+		{
+			name:     "different types in slices",
+			expected: []int{1, 2, 3},
+			actual:   []string{"1", "2", "3"},
+			want:     false,
+		},
+		{
+			name:     "different values in slices",
+			expected: []int{1, 2, 3},
+			actual:   []int{1, 2, 4},
+			want:     false,
+		},
+		{
+			name:     "same slices with different order",
+			expected: []int{1, 2, 3},
+			actual:   []int{3, 2, 1},
+			want:     false,
+		},
+		{
+			name:     "empty slices",
+			expected: []int{},
+			actual:   []int{},
+			want:     true,
+		},
+		{
+			name:     "same slices",
+			expected: []int{1, 2, 3},
+			actual:   []int{1, 2, 3},
+			want:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := exactMatch(tt.expected, tt.actual); got != tt.want {
+				t.Errorf("exactMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
